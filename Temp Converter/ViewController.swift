a431b9d91d2d8c502fb2b99b51699136c3780137
Omar Calles <omar@prosperworks.com>
2019-06-12 10:27:41 -0700

    Initial Commit

---
+++ Temp Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Temp Converter
+//
+//  Created by Omar Calles on 6/12/19.
+//  Copyright © 2019 Omar Calles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
