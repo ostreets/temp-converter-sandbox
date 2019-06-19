7c2da661a95faf4d4208bbbf55de1e77eeafecd1
Omar Calles <omar@prosperworks.com>
2019-06-19 11:48:13 -0700

    Added Clear button as well as the hooked up the convertToCelsius/Fahrenheit functions to cases in Convert button IBAction function

--- Temp Converter/ViewController.swift
+++ Temp Converter/ViewController.swift
@@ -13,28 +13,61 @@ class ViewController: UIViewController {
     @IBOutlet weak var convertButton: UIButton!
     @IBOutlet weak var fahrenheitTextField: UITextField!
     @IBOutlet weak var celsiusTextField: UITextField!
+    @IBOutlet weak var clearButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
     @IBAction func convertTemp(sender: UIButton) {
-        let fahrenheit = fahrenheitTextField.text
-        let celsius = celsiusTextField.text
-    
-        if fahrenheitTextField.text == nil || celsiusTextField.text != "" {
-            convertToFahrenheit(celsius: Int(celsius))
-        }
-        else if celsiusTextField.text == nil || fahrenheitTextField.text != ""{
-            convertToCelcius(fahrenheit: Int(fahrenheit))
+        if fahrenheitTextField.text == "" && celsiusTextField.text == ""{
+            fahrenheitTextField.text = "0"
+            celsiusTextField.text = "0"
+            print("both fahrenheit and celsius are zero")
+        }else if fahrenheitTextField.text != "" && celsiusTextField.text == ""{
+            print("Convert fahrenheit to celsius")
+            //let value = Int(fahrenheitTextField.text ?? "") ?? 0
+            //example of optionals
+            if let textValue = fahrenheitTextField.text{
+                print("have text value")
+                if let intValue = Float(textValue){
+                    print("Have int value")
+                    let convertedCelsiusValue = convertToCelsius(fahrenheit: intValue)
+                    celsiusTextField.text = String(convertedCelsiusValue)
+                } else {
+                    print("Have no value")
+                }
+            } else {
+                print ("nil text value")
+            }
+            
+        }else if fahrenheitTextField.text == "" && celsiusTextField.text != ""{
+            print("Convert celsius to fahrenheit")
+            let value = Float(celsiusTextField.text ?? "") ?? 0
+            let convertedFahrenheitValue = convertToFahrenheit(celsius: value)
+            fahrenheitTextField.text = String(convertedFahrenheitValue)
+            
+        }else if fahrenheitTextField.text != "" && celsiusTextField.text != ""{
+            fahrenheitTextField.text = ""
+            celsiusTextField.text = ""
+            print("both fahrenheit and celsius are NOT zero")
         }
     }
     
-    func convertToCelcius(fahrenheit: Int) -> Int{
-            return Int(5/9 * (Double(fahrenheit) - 32))
+    @IBAction func clearFields(sender: UIButton) {
+        if fahrenheitTextField.text != "" && celsiusTextField.text == "" || fahrenheitTextField.text == "" && celsiusTextField.text != "" || fahrenheitTextField.text != "" && celsiusTextField.text != "" {
+            fahrenheitTextField.text = ""
+            celsiusTextField.text = ""
+            print("both fahrenheit and celsius cleared")
+        }
     }
     
-    func convertToFahrenheit(celsius: Int) -> Int{
-        return Int(5/9 * (Double(celsius)) + 32)
+    func convertToCelsius(fahrenheit: Float) -> Float{
+        return Float(5/9 * ((Float(fahrenheit) - 32)))
+    }
+    
+    func convertToFahrenheit(celsius: Float) -> Float{
+        return Float((9/5 * (Float(celsius))) + 32)
     }
 }
 
